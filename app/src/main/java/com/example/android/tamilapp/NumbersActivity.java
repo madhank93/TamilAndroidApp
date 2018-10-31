c54ec7b79c6bb2f7e161dcff2ff4c5796d0aad11
madhank93 <maddymadhan946@gmail.com>
2018-10-31 21:44:03 +0530

    Comments added

--- app/src/main/java/com/example/android/tamilapp/NumbersActivity.java
+++ app/src/main/java/com/example/android/tamilapp/NumbersActivity.java
@@ -17,13 +17,14 @@ public class NumbersActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
 
-        // Create an Arraylist of words
+        // Create an Arraylist of words (Data)
         ArrayList<String> words = new ArrayList<String>(Arrays.asList("One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve","Thirteen","Fourteen", "Fifteen"));
 
+        // Create an Array Adapter ( ArrayAdapter - adapter converts an ArrayList of objects into View items loaded into the ListView container)
         ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, words);
 
+        // Connect the adapter to a ListView
         ListView listView = (ListView) findViewById(R.id.list);
-
         listView.setAdapter(itemsAdapter);
     }
 }
